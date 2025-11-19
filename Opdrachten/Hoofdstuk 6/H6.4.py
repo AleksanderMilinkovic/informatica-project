1e03927dab999efe41b4b1c32f5edebdff21bf6d
Aleksandertje1 <AleksanderMilinkovic@proton.me>
2025-11-19 19:17:46 +0100

    Waarschuwing voor onlogische invoer toegevoegd

--- Opdrachten/Hoofdstuk 6/H6.4.py
+++ Opdrachten/Hoofdstuk 6/H6.4.py
@@ -1,5 +1,10 @@
 from random import randint
 
+gameover = False
+getalTeGroot = False
+geraden = 0
+controleGetal = 0
+
 def getalKiezen():
     getal1 = int(input("Wat moet het laagste getal zijn waaruit de computer moet kiezen? "))
     getal2 = int(input("Wat moet het hoogste getal zijn waaruit de computer moet kiezen? "))
@@ -8,21 +13,29 @@ def getalKiezen():
     return randint(getal1, getal2)
 
 computerGetal = getalKiezen()
-gameover = False
-geraden = 0
 
 while not gameover:
     gebruikerGetal = int(input('Raad een getal: '))
+
+    while gebruikerGetal <= controleGetal and getalTeGroot == False:
+        gebruikerGetal = int(input(f'Je volgende poging moet groter zijn dan {controleGetal}! '))
+
+    while gebruikerGetal >= controleGetal and getalTeGroot == True:
+        gebruikerGetal = int(input(f'Je volgende poging moet kleiner zijn dan {controleGetal}! '))
 
     if computerGetal < gebruikerGetal:
         geraden += 1
         print('Het getal van de computer is kleiner!')
         print(f'Aantal keer geraden: {geraden}')
+        controleGetal = gebruikerGetal
+        getalTeGroot = True
 
     elif computerGetal > gebruikerGetal:
         geraden += 1
         print('Het getal van de computer is groter!')
         print(f'Aantal keer geraden: {geraden}')
+        controleGetal = gebruikerGetal
+        getalTeGroot = False
 
     else:
         geraden += 1
@@ -31,5 +44,5 @@ while not gameover:
         gameover = True
 
     if geraden >= 8 and gameover == False:
-        print('Game over')
+        print(f'Game over! Het getal was {computerGetal}')
         gameover = True

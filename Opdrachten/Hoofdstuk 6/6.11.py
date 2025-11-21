efd442e4d6cbcc1874d8e1ac14478787735cc9eb
Aleksandertje1 <AleksanderMilinkovic@proton.me>
2025-11-21 13:02:45 +0100

    Verbeteringen voor D <= 0

--- Opdrachten/Hoofdstuk 6/6.11.py
+++ Opdrachten/Hoofdstuk 6/6.11.py
@@ -6,5 +6,10 @@ c = int(input('Welke waarde heeft c? '))
 D = b**2 - 4*a*c
 
 print(f'D: {D}')
-print(f'x1: {-b+D**0.5/2*a}')
-print(f'x2: {(-b-D**0.5)/(2*a)}')
+if D < 0:
+    print("Deze vergelijking heeft geen oplossingen!")
+elif D == 0:
+    print(f'x: {-b/2*a}')
+else:
+    print(f'x1: {-b + D ** 0.5 / 2 * a}')
+    print(f'x2: {(-b - D ** 0.5) / (2 * a)}')

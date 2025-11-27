20a205eed95c7720ffa379915dbf7089758cf7a6
Aleksandertje1 <AleksanderMilinkovic@proton.me>
2025-11-27 12:39:01 +0100

    Fix voor fout in berekening (6.11)

--- Opdrachten/Hoofdstuk 6/6.11.py
+++ Opdrachten/Hoofdstuk 6/6.11.py
@@ -5,11 +5,13 @@ b = float(input('Welke waarde heeft b? '))
 c = float(input('Welke waarde heeft c? '))
 D = b**2 - 4*a*c
 
-print(f'D: {D}')
+print(f'D = {D}')
 if D < 0:
     print("Deze vergelijking heeft geen oplossingen!")
 elif D == 0:
-    print(f'x: {-b/2*a}')
+    x = -b/(2*a)
+    print(f'x = {x}')
 else:
-    print(f'x1: {-b+D**0.5 / 2*a}')
-    print(f'x2: {-b-D**0.5 / 2*a}')
+    x1 = round((-b - D**0.5) / (2*a), 2)
+    x2 = round((-b + D**0.5) / (2*a), 2)
+    print(f'x = {x1} V x = {x2}')

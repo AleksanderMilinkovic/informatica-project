e6fedb086d86ccbe378f7a287c637cefaa6dc0a1
Aleksandertje1 <AleksanderMilinkovic@proton.me>
2025-11-27 12:26:02 +0100

    Initiële omschrijving naar functie (6.7)

--- Opdrachten/Hoofdstuk 6/6.7.py
+++ Opdrachten/Hoofdstuk 6/6.7.py
@@ -2,23 +2,41 @@ import math
 
 oliebollen = int(input('Hoeveel oliebollen wil je bakken? '))
 recept = math.ceil(oliebollen / 20)
-melk = 500 * recept
+melkHoeveelheid = 500 * recept
+melkPakken = math.ceil(melkHoeveelheid/1000)
+eierDozen = math.ceil(recept/3)
+
+def ingredienten(product, hoeveelheid, hoeveelheid2):
+    if product == "melk":
+        if hoeveelheid2 == 1:
+            return f'Je hebt {hoeveelheid} ml melk nodig. In één verpakking zit 1000 ml melk.\nJe hebt 1 pak melk nodig.'
+        else:
+            return f'Je hebt {hoeveelheid} ml melk nodig. In één verpakking zit 1000 ml melk.\nJe hebt {hoeveelheid2} pakken melk nodig.'
+
+    elif product == "gist":
+        if hoeveelheid2 == 1:
+            return f'Je hebt {hoeveelheid} zakjes gist nodig. In één verpakking zitten 3 zakjes.\nJe hebt 1 pak gist nodig.'
+        else:
+            return f'Je hebt {hoeveelheid} zakjes gist nodig. In één verpakking zitten 3 zakjes.\nJe hebt {hoeveelheid2} pakken gist nodig.'
+
+    elif product == "ei":
+        if hoeveelheid2 == 1:
+            return f'Je hebt {hoeveelheid} stuks ei nodig. In één doos zitten 6 eieren.\nJe hebt 1 doos eieren nodig.'
+        else:
+            return f'Je hebt {hoeveelheid} stuks ei nodig. In één doos zitten 6 eieren. \nJe hebt {hoeveelheid2} eierdozen nodig.'
+
+    else:
+        return "Skibidi toilet ):"
+
 
 print(f'Je hebt ingrediënten nodig voor {20 * recept} oliebollen, anders kloppen de verhoudingen niet.\n')
 print('Ingrediëntenlijst:')
-print(f'Melk\nJe hebt {melk} ml melk nodig. In één verpakking zit 1000 ml melk.')
-print(f'Je hebt {math.ceil(melk/1000)} pakken melk nodig.\n')
+print(f'Melk\n')
+print(ingredienten(melk, melkHoeveelheid, melkPakken))
 
-print(f'Gist\nJe hebt {recept} zakjes gist nodig. In één verpakking zitten 3 zakjes.')
-print(f'Je hebt {math.ceil(recept/3)} verpakkingen gist nodig.\n')
-
-print(f'Ei\nJe hebt {recept} stuks ei nodig. In één verpakking zitten 6 stuks.')
-print(f'Je hebt {math.ceil(recept/3)} verpakkingen ei nodig.\n')
+print(f'Gist\n')
+print(ingredienten(ei, recept, eierDozen))
 
 print(f'Bloem\nJe hebt {500 * recept} g bloem nodig. In één verpakking zit 500 g bloem.')
 print(f'Je hebt {math.ceil(recept/3)} pakken bloem nodig.\n')
 print('Suiker, zout en zonnebloemolie heb je al genoeg.')
-
-"""
-def ingrediënten(hoeveelheid, product):
-"""

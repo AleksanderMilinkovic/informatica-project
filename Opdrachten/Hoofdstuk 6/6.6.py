c4e7ef35e74ae4e0e1408d3cdf35832599254fca
Aleksandertje1 <AleksanderMilinkovic@proton.me>
2025-11-28 09:35:38 +0100

    Opdracht afgemaakt, klaar voor inleveren (6.6)

--- Opdrachten/Hoofdstuk 6/6.6.py
+++ Opdrachten/Hoofdstuk 6/6.6.py
@@ -1,12 +1,12 @@
-"""
-Verboden scores:
-23, 29, 31, 35, 37, 41, 43, 44, 46, 47, 49, 52, 53, 55, 56, 58, 59, >60
-"""
+verbodenScore = [23, 29, 31, 35, 37, 41, 43, 44, 46, 47, 49, 52, 53, 55, 56, 58, 59]
+gameover = False
 beurt = 1
 punten = 501
 
 def vraagScore(pijl):
     score = int(input(f'Score van pijl {pijl}: '))
+    while score in verbodenScore or score > 60:
+        score = int(input('Dat is geen geldige score! Voor een geldige score in: '))
     return score
 
 while not gameover:
@@ -16,10 +16,15 @@ while not gameover:
     pijl1 = vraagScore(1)
     pijl2 = vraagScore(2)
     pijl3 = vraagScore(3)
-    beurt += 1
 
-    if pijl1 + pijl2 + pijl3 <= punten:
+    if pijl1 + pijl2 + pijl3 < punten:
         punten = punten - pijl1 - pijl2 - pijl3
+    elif pijl1 + pijl2 + pijl3 == punten:
+        if beurt == 3:
+            print('Je hebt een 9-darter!')
+        else:
+            print('Yes, uit!')
+        gameover = True
     else:
         print(f'Score is nu {punten - pijl1 - pijl2 - pijl3}, dus deze beurt telt niet!')
-
+    beurt += 1

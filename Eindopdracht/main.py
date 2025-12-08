0d531c855417f7561d989491606e03b1c619ba63
Aleksandertje1 <AleksanderMilinkovic@proton.me>
2025-12-08 15:54:27 +0100

    Extra informatie toegevoegd over specifieke eindopdracht

--- Eindopdracht/main.py
+++ Eindopdracht/main.py
@@ -4,4 +4,11 @@ Vereisten: Een Python-programma dat:
 - gebruikersinteracties simuleert via input en foutmeldingen;
 - een voorraad beheert; - geldinvoer verwerkt en foutafhandeling toepast;
 - logging en statistische analyse uitvoert na simulatie met meerdere gebruikers.
+
+Opdracht A: snackautomaat
+4 producten
+Gebruiker kiest product, voert geld in
+Controle, wisselgeld, voorraad aanpassen
+20 willekeurige klanten simuleren
+Overzicht: aantal verkopen per product, omzet en fouten
 """

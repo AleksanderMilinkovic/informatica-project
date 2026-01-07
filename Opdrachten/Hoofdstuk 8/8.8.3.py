7cfcde2adab07105dc858aae55f8cf21772e5e94
Aleksandertje1 <AleksanderMilinkovic@proton.me>
2026-01-07 13:13:06 +0100

    Spellogica van hoofdstuk 8 opdracht 3 grotendeels af

--- Opdrachten/Hoofdstuk 8/8.8.3.py
+++ Opdrachten/Hoofdstuk 8/8.8.3.py
@@ -14,14 +14,20 @@ def buttonKlik(name):
             geraden += 1
             if computerGetal < gebruikersGetal:
                 app.setLabel("Status", "Het getal van de computer is kleiner")
+                app.setImage("Hangman", f'hangman{geraden}.png')
             elif computerGetal > gebruikersGetal:
                 app.setLabel("Status", "Het getal van de computer is groter")
+                app.setImage("Hangman", f'hangman{geraden}.png')
             else:
                 app.setLabel("Status", "Je hebt het getal geraden!")
                 gameover = True
 
+            if geraden >= 8 and gameover == False:
+                print(f'Game over! Het getal was {computerGetal}')
+                gameover = True
+
     elif name == "Reset":
-        # Resetproces
+        app.stop()
 
 
 computerGetal = random.randint(1, 100)
@@ -29,5 +35,6 @@ app.addLabel("Titel", "Raad het getal tussen 0 en 100!", 0, 2)
 app.addLabel("Status", "", 1, 1)
 app.addEntry("invoer", 2, 0)
 app.addButtons(["Raden", "Reset"], buttonKlik, 3, 1)
+app.addImage("Hangman", "hangman0.gif")
 
 app.go()

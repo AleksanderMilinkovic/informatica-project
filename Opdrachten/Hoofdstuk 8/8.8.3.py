6d0c1aef1db8d97d556fa6964203adb1c394b0d9
Aleksander Milinković <158559723+AleksanderMilinkovic@users.noreply.github.com>
2026-01-07 17:18:23 +0000

    Hoofdstuk 8 opdracht 3 waarschijnlijk af

--- Opdrachten/Hoofdstuk 8/8.8.3.py
+++ Opdrachten/Hoofdstuk 8/8.8.3.py
@@ -1,40 +1,49 @@
-from appjar import gui
-from random import randint
+from appJar import gui
+import random
 
-app = gui("Hoger/lager")
+app = gui("Hoger/lager", "600x400")
+gameover = False
+geraden = 0
 
 def buttonKlik(name):
-    global computerGetal
-    gameover = False
-    geraden = 0
+    global computerGetal, gameover, geraden
 
     if name == "Raden":
-        while not gameover:
-            gebruikersGetal = int(str(app.getEntry("Raden")))
+        if not gameover:
+            gebruikersGetal = int(str(app.getEntry("Invoer")))
             geraden += 1
+            afbeelding = f'hangman{geraden}.gif'
+            
             if computerGetal < gebruikersGetal:
                 app.setLabel("Status", "Het getal van de computer is kleiner")
-                app.setImage("Hangman", f'hangman{geraden}.png')
+                app.setImage("Hangman", afbeelding)
+                app.clearEntry("Invoer")
             elif computerGetal > gebruikersGetal:
                 app.setLabel("Status", "Het getal van de computer is groter")
-                app.setImage("Hangman", f'hangman{geraden}.png')
+                app.setImage("Hangman", afbeelding)
+                app.clearEntry("Invoer")
             else:
                 app.setLabel("Status", "Je hebt het getal geraden!")
                 gameover = True
 
-            if geraden >= 8 and gameover == False:
-                print(f'Game over! Het getal was {computerGetal}')
+            if geraden >= 9 and not gameover:
+                app.setLabel("Status", f'Game over! Het getal was {computerGetal}')
                 gameover = True
 
     elif name == "Reset":
-        app.stop()
+        app.setLabel("Status", "")
+        app.setImage("Hangman", "hangman0.gif")
+        app.clearEntry("Invoer")
+        computerGetal = random.randint(1, 100)
+        gameover = False
+        geraden = 0
 
 
 computerGetal = random.randint(1, 100)
-app.addLabel("Titel", "Raad het getal tussen 0 en 100!", 0, 2)
-app.addLabel("Status", "", 1, 1)
-app.addEntry("invoer", 2, 0)
-app.addButtons(["Raden", "Reset"], buttonKlik, 3, 1)
-app.addImage("Hangman", "hangman0.gif")
+app.addLabel("Titel", "Raad het getal tussen 0 en 100!", 0, 0)
+app.addLabel("Status", "", 1, 0)
+app.addEntry("Invoer", 2, 0)
+app.addButtons(["Raden", "Reset"], buttonKlik, 5, 0)
+app.addImage("Hangman", "hangman0.gif", 2, 2)
 
 app.go()
